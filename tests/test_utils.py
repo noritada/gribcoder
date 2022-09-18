f67202a1375a67c2c79ac32bc23d2bc0a999ccca
Noritada Kobayashi <noritada.kobayashi@gmail.com>
2022-09-18 18:11:14 +0900

    Remove functions that are replaced and no longer needed

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,7 +1,6 @@
-import numpy as np
 import pytest
 
-from gribgen.utils import bytes_from_float, bytes_from_int, create_sect_header, grib_int
+from gribgen.utils import create_sect_header, grib_int
 
 
 def test_sect_header_creation():
@@ -20,27 +19,3 @@ def test_sect_header_creation():
 def test_grib_int(input_, byte_length, expected):
     actual = grib_int(input_, byte_length)
     assert actual == expected
-
-
-def test_bytes_from_positive_int():
-    input_ = 1024
-    length = 8
-    actual = bytes_from_int(input_, length)
-    expected = np.array([0, 0, 0, 0, 0, 0, 4, 0])
-    np.testing.assert_array_equal(actual, expected)
-
-
-def test_bytes_from_negative_int():
-    input_ = -1024
-    length = 8
-    actual = bytes_from_int(input_, length)
-    expected = np.array([128, 0, 0, 0, 0, 0, 4, 0])
-    np.testing.assert_array_equal(actual, expected)
-
-
-def test_bytes_from_float():
-    input_ = 1.0
-    length = 4
-    actual = bytes_from_float(input_, length)
-    expected = np.array([0x3F, 0x80, 0x00, 0x00])
-    np.testing.assert_array_equal(actual, expected)
