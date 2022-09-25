4e5ba73b6c52c0117999599f4af3fe43a2d98328
Noritada Kobayashi <noritada.kobayashi@gmail.com>
2022-09-25 23:31:40 +0900

    Implement Section 3 octet sequence writing

--- gribgen/grid.py
+++ gribgen/grid.py
@@ -1,6 +1,48 @@
 import dataclasses
+from typing import BinaryIO
 
 import numpy as np
+
+from gribgen.utils import SECT_HEADER_DTYPE, create_sect_header, grib_signed
+
+DTYPE_SHAPE_OF_THE_EARTH = np.dtype(
+    [
+        ("shape_of_the_earth", "u1"),
+        ("scale_factor_of_radius_of_spherical_earth", "u1"),
+        ("scaled_value_of_radius_of_spherical_earth", ">u4"),
+        ("scale_factor_of_earth_major_axis", "u1"),
+        ("scaled_value_of_earth_major_axis", ">u4"),
+        ("scale_factor_of_earth_minor_axis", "u1"),
+        ("scaled_value_of_earth_minor_axis", ">u4"),
+    ]
+)
+
+DTYPE_SECTION_3 = np.dtype(
+    [
+        ("source_of_grid_definition", "u1"),
+        ("number_of_data_points", ">u4"),
+        ("number_of_octets_for_number_of_points", "u1"),
+        ("interpretation_of_number_of_points", "u1"),
+        ("grid_definition_template_number", ">u2"),
+    ]
+)
+
+DTYPE_TEMPLATE_3_0_MAIN = np.dtype(
+    [
+        ("n_i", ">u4"),
+        ("n_j", ">u4"),
+        ("basic_angle_of_the_initial_production_domain", ">u4"),
+        ("subdivisions_of_basic_angle", ">u4"),
+        ("latitude_of_first_grid_point", ">u4"),  # grib_signed
+        ("longitude_of_first_grid_point", ">u4"),  # grib_signed
+        ("resolution_and_component_flags", "u1"),
+        ("latitude_of_last_grid_point", ">u4"),  # grib_signed
+        ("longitude_of_last_grid_point", ">u4"),  # grib_signed
+        ("i_direction_increment", ">u4"),
+        ("j_direction_increment", ">u4"),
+        ("scanning_mode", "u1"),
+    ]
+)
 
 _UNIT_DEG = 1_000_000
 
@@ -13,8 +55,8 @@ class LatitudeLongitudeGrid:
     last_lon: int
     num_grid_lat: int
     num_grid_lon: int
-    inc_lat: int
-    inc_lon: int
+    inc_lat: int | None
+    inc_lon: int | None
     scan_flag: int
 
     @classmethod
@@ -81,3 +123,69 @@ class LatitudeLongitudeGrid:
             raise RuntimeError(
                 "construction from ndarrays othar than 2d arrays is not supported"
             )
+
+    def shape_of_the_earth(
+        self, values: np.ndarray
+    ):  # `-> Self` for Python >=3.11 (PEP 673)
+        if values.dtype != DTYPE_SHAPE_OF_THE_EARTH:
+            raise RuntimeError("wrong dtype")
+        if len(values) != 1:
+            raise RuntimeError("wrong length")
+        self._shape_of_the_earth = values
+        return self
+
+    def write(self, f: BinaryIO) -> int:
+        section_main_buf = np.array(
+            [
+                (
+                    0,  # using lat/lon grid in code table 3.1
+                    self.num_grid_lat * self.num_grid_lon,
+                    0,  # fixed for regular grids
+                    0,  # fixed for regular grids
+                    0,  # lat/lon grid
+                )
+            ],
+            dtype=DTYPE_SECTION_3,
+        )
+
+        template_main_buf = np.array(
+            [
+                (
+                    self.num_grid_lon,
+                    self.num_grid_lat,
+                    0,
+                    0xFFFFFFFF,
+                    grib_signed(self.first_lat, 4),
+                    grib_signed(self.first_lon, 4),
+                    self._get_resolution_and_component_flag(),
+                    grib_signed(self.last_lat, 4),
+                    grib_signed(self.last_lon, 4),
+                    self.inc_lon,
+                    self.inc_lat,
+                    self.scan_flag,
+                )
+            ],
+            dtype=DTYPE_TEMPLATE_3_0_MAIN,
+        )
+
+        sect_len = (
+            SECT_HEADER_DTYPE.itemsize
+            + DTYPE_SECTION_3.itemsize
+            + DTYPE_SHAPE_OF_THE_EARTH.itemsize
+            + DTYPE_TEMPLATE_3_0_MAIN.itemsize
+        )
+
+        header = create_sect_header(3, sect_len)
+        f.write(header)
+        f.write(section_main_buf)
+        f.write(self._shape_of_the_earth)
+        f.write(template_main_buf)
+        return sect_len
+
+    def _get_resolution_and_component_flag(self) -> int:
+        flag = 0b00000000
+        if self.inc_lat is not None:
+            flag |= 0b00010000
+        if self.inc_lon is not None:
+            flag |= 0b00100000
+        return flag
--- tests/test_grid.py
+++ tests/test_grid.py
@@ -1,7 +1,10 @@
+from io import BytesIO
+
+import helpers
 import numpy as np
 import pytest
 
-from gribgen.grid import LatitudeLongitudeGrid
+from gribgen.grid import DTYPE_SHAPE_OF_THE_EARTH, LatitudeLongitudeGrid
 
 
 @pytest.mark.parametrize(
@@ -134,4 +137,85 @@ def test_lat_lon_grid_parameter_extraction_from_ndarrays(
 ):
     actual = LatitudeLongitudeGrid.from_ndarrays(input_lat, input_lon)
     expected = LatitudeLongitudeGrid(**expected_params)
+    assert actual == expected
+
+
+@pytest.mark.parametrize(
+    "input,expectation,error_message",
+    [
+        (
+            np.array([(0, 0, 0, 0, 0, 0, 0)], dtype=DTYPE_SHAPE_OF_THE_EARTH),
+            helpers.does_not_raise(),
+            None,
+        ),
+        (
+            np.array([0]),
+            pytest.raises(RuntimeError),
+            "wrong dtype",
+        ),
+        (
+            np.array(
+                [(0, 0, 0, 0, 0, 0, 0), (1, 1, 1, 1, 1, 1, 1)],
+                dtype=DTYPE_SHAPE_OF_THE_EARTH,
+            ),
+            pytest.raises(RuntimeError),
+            "wrong length",
+        ),
+    ],
+)
+def test_setting_shape_of_the_earth_to_lat_lon_grid(input, expectation, error_message):
+    dummy_params = {
+        "first_lat": 10_000_000,
+        "first_lon": 0,
+        "last_lat": 19_000_000,
+        "last_lon": 48_000_000,
+        "num_grid_lat": 10,
+        "num_grid_lon": 25,
+        "inc_lat": 1_000_000,
+        "inc_lon": 2_000_000,
+        "scan_flag": 0b00000000,
+    }
+    grid = LatitudeLongitudeGrid(**dummy_params)
+    with expectation as e:
+        grid.shape_of_the_earth(input)
+
+    if e is not None:
+        assert str(e.value) == error_message
+
+
+@pytest.mark.parametrize(
+    "input_params,shape_of_the_earth_params,expected",
+    [
+        (
+            # params for JMA MSM GRIB2
+            {
+                "first_lat": 47_975_000,
+                "first_lon": 120_031_250,
+                "last_lat": 20_025_000,
+                "last_lon": 149_968_750,
+                "num_grid_lat": 560,
+                "num_grid_lon": 480,
+                "inc_lat": 50_000,
+                "inc_lon": 62_500,
+                "scan_flag": 0b00000000,
+            },
+            np.array(
+                [(6, 0xFF, 0xFFFFFFFF, 0xFF, 0xFFFFFFFF, 0xFF, 0xFFFFFFFF)],
+                dtype=DTYPE_SHAPE_OF_THE_EARTH,
+            ),
+            b"\x00\x00\x00\x48\x03\x00\x00\x04\x1a\x00\x00\x00\x00\x00\x06\xff"
+            + b"\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\x00\x00"
+            + b"\x01\xe0\x00\x00\x02\x30\x00\x00\x00\x00\xff\xff\xff\xff\x02\xdc"
+            + b"\x0a\x58\x07\x27\x88\x12\x30\x01\x31\x8e\xa8\x08\xf0\x57\x6e\x00"
+            + b"\x00\xf4\x24\x00\x00\xc3\x50\x00",
+        ),
+    ],
+)
+def test_sect7_writing(input_params, shape_of_the_earth_params, expected):
+    grid = LatitudeLongitudeGrid(**input_params).shape_of_the_earth(
+        shape_of_the_earth_params
+    )
+    with BytesIO() as f:
+        grid.write(f)
+        actual = f.getvalue()
     assert actual == expected
