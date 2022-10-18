f406d5a5a049bba5e0e5d8f3f6e2985c2cc5b0e1
Noritada Kobayashi <noritada.kobayashi@gmail.com>
2022-10-18 13:40:11 +0900

    Export dtype constants in the API

--- gribgen/__init__.py
+++ gribgen/__init__.py
@@ -1,15 +1,27 @@
 from .context import Grib2MessageWriter
 from .encoders import BaseEncoder, SimplePackingEncoder
-from .grid import BaseGrid, LatitudeLongitudeGrid
+from .grid import DTYPE_SHAPE_OF_THE_EARTH, BaseGrid, LatitudeLongitudeGrid
 from .message import Identification, Indicator
-from .product import BaseProductDefinition, ProductDefinitionWithTemplate4_0
+from .product import (
+    DTYPE_SECTION_4_FORECAST_TIME,
+    DTYPE_SECTION_4_GENERATING_PROCESS,
+    DTYPE_SECTION_4_HORIZONTAL,
+    DTYPE_SECTION_4_PARAMETER,
+    BaseProductDefinition,
+    ProductDefinitionWithTemplate4_0,
+)
 
 __all__ = [
     "Grib2MessageWriter",
     "BaseEncoder",
     "SimplePackingEncoder",
+    "DTYPE_SHAPE_OF_THE_EARTH",
     "BaseGrid",
     "LatitudeLongitudeGrid",
+    "DTYPE_SECTION_4_FORECAST_TIME",
+    "DTYPE_SECTION_4_GENERATING_PROCESS",
+    "DTYPE_SECTION_4_HORIZONTAL",
+    "DTYPE_SECTION_4_PARAMETER",
     "BaseProductDefinition",
     "ProductDefinitionWithTemplate4_0",
     "Identification",
