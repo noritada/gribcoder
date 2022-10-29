f85fab943eb73afc4bfb4a8eaf90148273103296
Noritada Kobayashi <noritada.kobayashi@gmail.com>
2022-10-29 14:03:41 +0900

    Improve an example script

--- examples/generation.py
+++ examples/generation.py
@@ -15,14 +15,14 @@ class Element:
 
 
 ELEMENTS = [
-    Element("REFD", 15, 1),
+    Element("TEMP", 0, 0),
 ]
 
 
 def generate_grib2(grid_lats, grid_lons, grid_alts, regridded, time):
     with BytesIO() as fw:
         ind = gribcoder.Indicator(0)
-        ident = gribcoder.Identification(0xFFFF, 0xFFFF, 29, 0, 0, time, 0, 0)
+        ident = gribcoder.Identification(0xFFFF, 0xFFFF, 29, 0, 0, time, 0xFF, 0)
         with gribcoder.Grib2MessageWriter(fw, ind, ident) as grib2:
             grid = gribcoder.LatitudeLongitudeGrid.from_ndarrays(
                 grid_lats[:, :, 0], grid_lons[:, :, 0]
@@ -36,7 +36,7 @@ def generate_grib2(grid_lats, grid_lons, grid_alts, regridded, time):
 
             n_alt = grid_alts.shape[2]
             for i in range(n_alt):
-                alt = grid_alts[0, 0, i]
+                alt = 0
                 for elem_index, elem in enumerate(ELEMENTS):
                     if elem is None:
                         continue
@@ -62,7 +62,7 @@ def generate_grib2(grid_lats, grid_lons, grid_alts, regridded, time):
                         )
                         .horizontal(
                             np.array(
-                                [(102, 0, alt, 0xFF, 0xFF, 0xFFFFFFFF)],
+                                [(101, 0, alt, 0xFF, 0xFF, 0xFFFFFFFF)],
                                 dtype=gribcoder.DTYPE_SECTION_4_HORIZONTAL,
                             )
                         )
@@ -81,8 +81,8 @@ def generate_grib2(grid_lats, grid_lons, grid_alts, regridded, time):
     return output
 
 
-grid_lats = np.tile(np.arange(5), (2, 1)).reshape(2, -1, 1)
-grid_lons = np.tile(np.arange(2), (5, 1)).T.reshape(-1, 5, 1)
+grid_lats = np.tile(np.arange(3), (2, 1)).reshape(2, -1, 1)
+grid_lons = np.tile(np.arange(2), (3, 1)).T.reshape(-1, 3, 1)
 grid_alts = np.ones_like(grid_lats)
 x, y, z = grid_lats.shape
 length = x * y * z
@@ -94,7 +94,24 @@ print(values[:, :, 0])
 
 
 grib2 = generate_grib2(
-    grid_lats, grid_lons, grid_alts, values.reshape(2, 5, 1, 1), datetime.now()
+    grid_lats,
+    grid_lons,
+    grid_alts,
+    values.reshape(2, 3, 1, 1),
+    datetime(2022, 10, 1, 0, 0, 0),
 )
 with open("generated.grib", "wb") as fw:
+    fw.write(grib2)
+
+values = values.astype(float)
+values[0, 0, 0] = np.nan
+values = np.ma.masked_invalid(values)
+grib2 = generate_grib2(
+    grid_lats,
+    grid_lons,
+    grid_alts,
+    values.reshape(2, 3, 1, 1),
+    datetime(2022, 10, 1, 0, 0, 0),
+)
+with open("generated_with_bitmap.grib", "wb") as fw:
     fw.write(grib2)
